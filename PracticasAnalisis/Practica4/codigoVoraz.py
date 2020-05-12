30be4428d0d0be27b6d760c9e5defdd44cc950a3
EdgarMung <lamborguini720@gmail.com>
2020-05-12 18:37:51 -0500

    CReaciones de las listas

--- PracticasAnalisis/Practica4/codigoVoraz.py
+++ PracticasAnalisis/Practica4/codigoVoraz.py
@@ -1,18 +1,29 @@
 matrices = [[30,35],[35,15],[15,5],[5,10],[10,20],[20,25]]
 
-def InsertionSort():
-    global Lista_1
-    n = len(Lista_1)
+def InsertionSort(Elementos):
+    n = len(Elementos)
     for i in range(1,n):
         j = i - 1
-        while j >= 0 and Lista_1[j][1] < Lista_1[j+1][1]:
-            swap(Lista_1,j,j+1)
+        while j >= 0 and Elementos[j][1] < Elementos[j+1][1]:
+            swap(Elementos,j,j+1)
             j = j-1
 
+    Elementos_1 = Elementos[:]
+
+    for i in range(1,n):
+        j = i - 1
+        while j >= 0 and Elementos[j][0] < Elementos[j+1][0]:
+            swap(Elementos,j,j+1)
+            j = j-1 
+    
+    Elementos_2 = Elementos[:]
+
+    return Elementos_1,Elementos_2
+
 def swap(Array,x,y):
-    aux = Array[x][1]
-    Array[x][1] = Array[y][1]
-    Array[y][1] = aux
+    aux = Array[x]
+    Array[x] = Array[y]
+    Array[y] = aux
 
 def ingresoDatos():
     print("\n  Ingresa las matrices separadas por comas ',' y meter las matrices con el formato AxB.")
@@ -27,15 +38,4 @@ def ingresoDatos():
         #intermediario_B.append(int)
         matrices.append([int(intermediario[0]),int(intermediario[1])])
 
-#ingresoDatos()
-
-Lista_1 = matrices[:]
-
-for i in Lista_1:
-    print(i)
-
-InsertionSort()
-
-print("------------")
-for i in Lista_1:
-    print(i)
+Lista_1,Lista_2 = InsertionSort(matrices)

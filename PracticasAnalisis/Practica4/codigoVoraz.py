0491d11c199df10f0765631d1a89508dbac30579
EdgarMung <lamborguini720@gmail.com>
2020-05-18 10:50:25 -0500

    Finalizacion parte voraz, inicio parte dinamica

--- PracticasAnalisis/Practica4/codigoVoraz.py
+++ PracticasAnalisis/Practica4/codigoVoraz.py
@@ -1,5 +1,56 @@
 matrices = [[30,35],[35,15],[15,5],[5,10],[10,20],[20,25]]
 
+def SegundoFiltro(Elementos_1,Elementos_2):
+    global suma
+    posicion = -1
+
+    while len(Elementos_1) + posicion >= 0 :
+        bandera = 0
+        Aj = Elementos_1[posicion]
+
+        for Elemento in Elementos_1:
+            if Aj[0] == Elemento[1]:
+                Ak = Elemento
+                EliminarElemento(Lista_1,Aj,Ak)
+                EliminarElemento(Lista_2,Ak,Aj)
+                Elementos_1.append([Ak[0],Aj[1]])
+                suma+= Ak[0]*Ak[1]*Aj[1]
+                bandera = 1
+                break
+        if bandera == 0:
+            posicion -= 1
+
+def PrimerFiltro(Elementos_1,Elementos_2):
+    global suma
+    posicion = -1
+    
+    while len(Elementos_1) + posicion >= 0 :
+        bandera = 0
+        Aj = Elementos_1[posicion]
+
+        for Elemento in Elementos_2:
+            if Aj[0] == Elemento[1]:
+                Ak = Elemento
+                EliminarElemento(Lista_1,Aj,Ak)
+                EliminarElemento(Lista_2,Ak,Aj)
+                Elementos_1.append([Ak[0],Aj[1]])
+                suma+= Ak[0]*Ak[1]*Aj[1]
+                bandera = 1
+                break
+        if bandera == 0:
+            posicion -= 1
+
+def EliminarElemento(Lista,Elemento_1,Elemento_2):
+    for i in Lista:
+        if Elemento_1 == i:
+            Lista.remove(Elemento_1)
+            break
+    
+    for j in Lista:
+        if Elemento_2 == j:
+            Lista.remove(Elemento_2)
+            break
+
 def InsertionSort(Elementos):
     n = len(Elementos)
     for i in range(1,n):
@@ -30,7 +81,6 @@ def ingresoDatos():
     print("  Ejemplo: AxB,CxD,ExF,....")
 
     Entrada = input("\tIngresa las matrices: ")
-    #print(Entrada.split(","))
     aux = Entrada.split(",")
 
     for i in aux:
@@ -38,4 +88,22 @@ def ingresoDatos():
         #intermediario_B.append(int)
         matrices.append([int(intermediario[0]),int(intermediario[1])])
 
+suma = 0
+cadena = ""
+
 Lista_1,Lista_2 = InsertionSort(matrices)
+PrimerFiltro(Lista_1,Lista_2)
+SegundoFiltro(Lista_1,Lista_2)
+
+print("----------------")
+print("suma: "+str(suma))
+
+print("-----------------")
+print("Lista 1")
+for i in Lista_1:
+    print(i)
+
+print("------------------")
+print("Lista 2")
+for i in Lista_2:
+    print(i)
